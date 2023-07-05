c53f02a9d009fbec7035941217e6f471a4cf671f
thirumurugan <thirumurugan5922@gmail.com>
2023-07-05 13:45:32 +0530

    project structure creation

---
+++ student-management-systems/src/main/java/com/soft/studentmanagementsystems/StudentManagementSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.soft.studentmanagementsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemsApplication.class, args);
+	}
+
+}
---
+++ student-management-systems/src/test/java/com/soft/studentmanagementsystems/StudentManagementSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.studentmanagementsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
